0b22c2300d212439489fd8c0088fa6d11320d376
mostafa <mostafa>
2026-08-05 17:53:26 +0330

    adding route for edit product in admin the route is 'localhost:5000/admin/dashboard/edit-product/(product id)'

--- app/blueprints/admin.py
+++ app/blueprints/admin.py
@@ -57,3 +57,41 @@ def add_product():
             return "خطا " + "<br>" + "<a href='{{url_for(\"admin.dashboard\")}}'></a>"
     products = Product.query.all()
     return render_template("admin/products.html", products=products)
+
+
+@app.route("/dashboard/edit-product/<int:id>", methods = ["GET", "POST"])
+def edit_product(id):
+    product = Product.query.get(int(id))
+
+    if not product:
+        flash("محصول مورد نظر پیدا نشد", "warning")
+        return redirect(url_for("admin.dashboard"))
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        description = request.form.get("description")
+        stock = int(request.form.get("stock"))
+        price = int(request.form.get("price"))
+        active = request.form.get("active")
+
+        if active :
+            active = True
+        else:
+            active = False
+
+        try:
+            product.name = name
+            product.description = description
+            product.stock = stock
+            product.price = price
+            product.active = active
+
+            db.session.commit()
+            flash("محصول مورد نظر ویرایش شد", "info")
+            return redirect(url_for("admin.dashboard"))
+        except Exception as ex:
+            print(ex)
+            flash("سیستم دچار مشکل شده", "warning")
+            return redirect(url_for("admin.dashboard"))
+
+    return render_template("admin/edit-product.html", product=product)
