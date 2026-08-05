abe239b6962e5665ee399a1d2585cc35b9886150
mostafa <mostafa>
2026-08-05 20:24:49 +0330

    adding home page

--- app/blueprints/general.py
+++ app/blueprints/general.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, flash, redirect, url_for
+from app.models.product import Product
 
 app = Blueprint("general", __name__)
 
+
 @app.route("/")
 def home():
-    return "HOME"
+    products = Product.query.filter(Product.active == True).all()
+    return render_template("home.html", products=products)
